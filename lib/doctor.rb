dc99a254a36525b8447a827fe844bd07f03dbff0
Stela Capsa <stela.capsa@gmail.com>
2020-01-12 19:07:02 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,37 @@
+class Doctor 
+  attr_reader :name, :appointment, :patient
+
+  @@all_doc = []
+
+  def initialize(name)
+    @name = name
+    @@all_doc << self
+   
+  end
+
+  def self.all
+    @@all_doc
+  end
+  
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.doctor == self
+     end
+  end
+
+
+  def new_appointment(patient, date)
+    Appointment.new(patient, self, date)
+  
+  end
+
+ 
+
+  def patients
+    Appointments.collect do |appointment|
+      appointment.patients
+    end
+  end
+
+end 
+
