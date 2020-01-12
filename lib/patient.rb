27a7ce564a352983148797f3daee907c9d731713
Stela Capsa <stela.capsa@gmail.com>
2020-01-12 19:18:21 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,5 +1,5 @@
 class Patient
-  attr_accessor :anme 
+  attr_accessor :name 
   
   @@all = []
   def initialize(name)
@@ -24,7 +24,7 @@ class Patient
   end
 
   def doctors
-    Appointment.all.map do |appointment|
+    appointment.all.map do |appointment|
       appointment.doctor
     end
   end
