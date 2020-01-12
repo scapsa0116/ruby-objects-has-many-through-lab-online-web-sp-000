ac45a63457ad56e6cdda44ccdb280de14c8eab9c
Stela Capsa <stela.capsa@gmail.com>
2020-01-12 19:28:57 +0000

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,19 @@
+class Appointment
+  
+  attr_reader :patient, :doctor, :date
+
+  @@all = []
+
+  def initialize(date = "", patient ="", doctor ="")
+    @patient = patient
+    @doctor = doctor
+    @date = date
+    @@all << self
+  end
+
+def self.all 
+  @@all 
+end 
+  
+  
+end 
---
+++ lib/doctor.rb
@@ -0,0 +1,36 @@
+class Doctor 
+  attr_accessor :name
+
+  @@all_doc = []
+
+  def initialize(name)
+    @name = name
+    @@all_doc << self
+   
+  end
+
+  def self.all
+    @@all_doc
+  end
+  
+  def appointments
+    Appointment.all.select{|appointment| appointment.doctor == self}
+     
+  end
+
+
+  def new_appointment(patient, date)
+    Appointment.new(date, patient, self)
+  
+  end
+
+ 
+
+  def patients
+    appointments.map do |appointment|
+      appointment.patient
+    end
+  end
+
+end 
+
